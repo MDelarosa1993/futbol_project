4142e2ffd2ed1f462f5a698a23f1eb3b92d05c64
Terra Manning <terradmanning@gmail.com>
2024-08-01 16:14:13 -0600

    feat: add count tests, still nonfuncional

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -22,17 +22,25 @@ RSpec.describe StatTracker do
   end
 
   describe 'highest total score and lowest total' do 
-    xit 'returns highest score' do 
+    it 'returns highest score' do 
       expect(@stat_tracker.highest_total_score).to eq(11)
     end
   end
 
-  xit 'returns lowest score' do 
+  it 'returns lowest score' do 
     expect(@stat_tracker.lowest_total_score).to eq(0)
   end
 
-  xit 'calculates total score betwen highest and lowest score' do 
+  it 'calculates total score betwen highest and lowest score' do 
     total_scores = @stat_tracker.total_score
     expect(@stat_tracker.total_score).to eq(total_scores)
+  end
+
+  it 'counts games by season'do
+    expect(@stat_tracker.count_of_games_by_season).to eq()
+  end
+
+  it 'counts all unique teams' do
+    expect(@stat_tracker.count_of_teams).to eq()
   end
 end 
