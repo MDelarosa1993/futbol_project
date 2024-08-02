2cd4b7ce2ccb3e3da8f919e103992bbfaf78e297
Terra Manning <terradmanning@gmail.com>
2024-08-02 16:42:39 -0600

    feat: readd count and offense tests

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -47,4 +47,44 @@ RSpec.describe StatTracker do
   it "#percentage_ties" do
     expect(@stat_tracker.percentage_ties).to eq 0.20
   end
+
+  it "#average_goals_per_game" do
+    expect(@stat_tracker.average_goals_per_game).to eq 4.22
+  end
+
+  it "#average_goals_by_season" do
+    expected = {
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
+    }
+    expect(@stat_tracker.average_goals_by_season).to eq expected
+  end
+
+  it "#count_of_games_by_season" do
+  expected = {
+    "20122013"=>806,
+    "20162017"=>1317,
+    "20142015"=>1319,
+    "20152016"=>1321,
+    "20132014"=>1323,
+    "20172018"=>1355
+  }
+  expect(@stat_tracker.count_of_games_by_season).to eq expected
+end
+
+  it "#count_of_teams" do
+    expect(@stat_tracker.count_of_teams).to eq 32
+  end
+
+  xit "#best_offense" do
+    expect(@stat_tracker.best_offense).to eq "Reign FC"
+  end
+
+  xit "#worst_offense" do
+    expect(@stat_tracker.worst_offense).to eq "Utah Royals FC"
+  end
 end 
