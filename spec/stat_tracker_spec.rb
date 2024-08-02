2ab40f95022084130638358521f622d88730b4c4
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-08-02 13:59:22 -0400

    Tests for the methods

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -48,5 +48,19 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.percentage_ties).to eq 0.20
   end
 
-  
+  it "#average_goals_per_game" do
+    expect(@stat_tracker.average_goals_per_game).to eq 4.22
+  end
+
+  it "#average_goals_by_season" do
+    expected = {
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
+    }
+    expect(@stat_tracker.average_goals_by_season).to eq expected
+  end
 end 
